516265ae53465f8cde5d84c03ea7ff26320f507c
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-03-17 00:44:36 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 do
   it "contains a local variable called first_number that is assigned to a number" do
-first_number = "" ('./calculator.rb', "first_number")
+first_number = "" ("first_number")
 expect (first_number) .to be an (Integer) or be a (Float)
 end
 end
