555e4096d45ed0a83c0b7d4ee4dc0c38d1d1b8eb
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-03-17 00:35:10 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,4 @@
 it "contains a local variable called first_number that is assigned to a number" do
-first_number = " "
+first_number = "-1"
 expect (first_number) .to be an (Integer) or be a (Float)
 end
